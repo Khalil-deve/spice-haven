d103ea36348eb5016a6582fcd9f843317a016fe7
Khalil-deve <msamad09874821@gmail.com>
2026-07-11 00:56:30 +0500

    fix server issue

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from "express";
 // @ts-nocheck
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
 
